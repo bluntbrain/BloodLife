36953d1f6ada32c602920fe9dd2c9a4456ecec08
Lovish Badlani <42366382+LovishB@users.noreply.github.com>
2020-03-02 18:04:38 +0530

    Added requests activity with ripple effect

--- app/src/main/java/com/example/learningmaps/Request.java
+++ app/src/main/java/com/example/learningmaps/Request.java
@@ -3,23 +3,78 @@ package com.example.learningmaps;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.skyfishjy.library.RippleBackground;
+
+import java.util.ArrayList;
 
 public class Request extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private RippleBackground mRippleBackground;
+    private ImageView mImageView;
 
     BottomNavigationView mBottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request);
-
+        mImageView=findViewById(R.id.requests_backbutton);
+        mRippleBackground=findViewById(R.id.ripple);
+        mRippleBackground.startRippleAnimation();
         mBottomNavigationView=findViewById(R.id.bottomNavigation);
         mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        ArrayList<AddingItemsRequests> requestsArrayList=new ArrayList<>();
+        //adding data now
+        requestsArrayList.add(new AddingItemsRequests("Lovish Badlani","O+","mandavi emerald","9413119402","Critical","14"));
+        requestsArrayList.add(new AddingItemsRequests("Naman Sharma","AB+","block 14","9413119402","Emergency","12"));
+        requestsArrayList.add(new AddingItemsRequests("Anukriti Mehta","B+","mandavi emerald","9413119402","Critical","14"));
+        requestsArrayList.add(new AddingItemsRequests("Lovish Badlani","O+","mandavi emerald","9413119402","Critical","14"));
+        requestsArrayList.add(new AddingItemsRequests("Lovish Badlani","O+","mandavi emerald","9413119402","Critical","14"));
+        requestsArrayList.add(new AddingItemsRequests("Lovish Badlani","O+","mandavi emerald","9413119402","Critical","14"));
+        requestsArrayList.add(new AddingItemsRequests("Lovish Badlani","O+","mandavi emerald","9413119402","Critical","14"));
+        requestsArrayList.add(new AddingItemsRequests("Lovish Badlani","O+","mandavi emerald","9413119402","Critical","14"));
+
+        mRecyclerView=findViewById(R.id.recyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager=new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter= new RequestsCustomAdapter(requestsArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
