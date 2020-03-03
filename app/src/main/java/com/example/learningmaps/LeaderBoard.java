f22c33a948a8865806031540d4ee8c8b724443a2
Lovish Badlani <42366382+LovishB@users.noreply.github.com>
2020-03-03 16:07:12 +0530

    Effect added on dialogue pics leaderboard

--- app/src/main/java/com/example/learningmaps/LeaderBoard.java
+++ app/src/main/java/com/example/learningmaps/LeaderBoard.java
@@ -7,25 +7,58 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+import com.flaviofaria.kenburnsview.KenBurnsView;
+import com.flaviofaria.kenburnsview.Transition;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LeaderBoard extends AppCompatActivity {
-    BottomNavigationView mBottomNavigationView;
+    private BottomNavigationView mBottomNavigationView;
+    private KenBurnsView kbv ;
+    private int flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leader_board);
+        kbv= findViewById(R.id.kenburns);
 
         mBottomNavigationView=findViewById(R.id.bottomNavigation);
         mBottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        kbv.setTransitionListener(new KenBurnsView.TransitionListener() {
+            @Override
+            public void onTransitionStart(Transition transition) {
+
+            }
+
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                if(flag==0){
+                    YoYo.with(Techniques.FadeOut)
+                            .duration(1400)
+                            .playOn(findViewById(R.id.dialoglay));
+                    kbv.setImageResource(R.drawable.pictwo); flag++;
+                YoYo.with(Techniques.FadeIn)
+                        .duration(1400)
+                        .playOn(findViewById(R.id.dialoglay));}
+                else if(flag==1){
+                    kbv.setImageResource(R.drawable.picthree); flag++;}
+                else if(flag==2){
+                    kbv.setImageResource(R.drawable.picone); flag++;}
+                else if(flag==3){
+                    kbv.setImageResource(R.drawable.pictwo); flag++;}
+                else if(flag==4){
+                        kbv.setImageResource(R.drawable.picthree); flag=0;}
+
+
+
+
+
+            }
+        });
     }
-
-
-
-
-
-
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
